373cc6e904657edaff5c7b48b52a013b05b25ef5
yujinjjang2 <o3odw98@gmail.com>
2023-11-01 10:47:26 +0900

    update aca

--- boardProject/src/main/java/edu/kh/project/member/model/service/MemberServiceImpl.java
+++ boardProject/src/main/java/edu/kh/project/member/model/service/MemberServiceImpl.java
@@ -1,5 +1,7 @@
 package edu.kh.project.member.model.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,10 @@ import edu.kh.project.member.model.dto.Member;
 @Service // 비즈니스 로직(데이터 가공, DAO 호출, 트랜잭션 제어)처리하는 클래스 명시
 		// + Bean으로 등록하는 어노테이션
 public class MemberServiceImpl implements MemberService{
+	
+	// org.slf4j.Logger : 로그를 작성할 수 있는 객체
+	// org.slf4j.LoggerFactory                        // 현재 클래스명 넣어주어야 함! (현재 클래스에 logger 사용하겠다)
+	private Logger logger = LoggerFactory.getLogger(MemberServiceImpl.class);
 	
 	// @Autowired : 작성된 필드와
 	// Bean으로 등록된 객체 중 타입이 일치하는 Bean을
@@ -28,6 +34,13 @@ public class MemberServiceImpl implements MemberService{
 	
 	@Override
 	public Member login(Member inputMember) {
+		
+		// 로그 출력
+		logger.info("MemberServiceImpl.login() 실행"); // 정보 출력
+		logger.debug("memberEmail : " + inputMember.getMemberEmail() );
+		logger.warn("이건 경고 용도");
+		logger.error("이건 오류 발생 시");
+		
 		
 		// 암호화가 다 다른지 test 용도
 //		System.out.println("암호화 확인 1:"  +  bcrypt.encode(  inputMember.getMemberPw()  )  );
